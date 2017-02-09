fe86dfc89764d011d5db9d1f7f841cc99c33a819
AIE\s171372 <sonicxeps@gmail.com>
2017-02-09 13:32:20 +1100

    Added GetDirection Function to Bullet

--- project2D/Bullet.cpp
+++ project2D/Bullet.cpp
@@ -27,7 +27,7 @@ Bullet::~Bullet()
 
 void Bullet::Update(const float deltaTime)
 {
-	Vector2 velocity = GetDirection();
+	Vector2 velocity = BulletDirection();
 	velocity = (velocity * m_speed) * deltaTime;
 
 	m_position = m_position + velocity;
@@ -45,7 +45,12 @@ Vector2 Bullet::GetPosition()
 	return m_position;
 }
 
-const Vector2 Bullet::GetDirection()
+Direction Bullet::GetDirection()
+{
+	return m_direction;
+}
+
+const Vector2 Bullet::BulletDirection()
 {
 	// 1 = up | -1 = down
 	switch (m_direction)
